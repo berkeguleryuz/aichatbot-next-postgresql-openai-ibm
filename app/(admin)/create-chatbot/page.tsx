6dd9e21bf0598f43642a844650a941a6e62b7c8a
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-08-07 17:20:09 +0200

    stepzen updates

--- app/(admin)/create-chatbot/page.tsx
+++ app/(admin)/create-chatbot/page.tsx
@@ -6,14 +6,14 @@ import React from "react";
 const CreateChatbot = () => {
   return (
     <div className="flex flex-col items-center justify-center md:flex-row md:space-x-10 bg-white p-10 rounded-md m-10">
-      {/* <Avatar seed="create-chatbot" /> */}
+      <Avatar seed={"Clo"} />
       <div className="">
         <h1 className="text-xl lg:text-3xl font-semibold">Create</h1>
         <h2 className="font-light">
           Create your first chatbot with Clodron. It would be increase your
           customer experience.
         </h2>
-        <form className="flex flex-col md:flex-row" action="">
+        <form className="flex flex-col md:flex-row gap-2 mt-5" action="">
           <Input
             placeholder="Enter Your Chatbot's Name"
             className="max-w-lg"
